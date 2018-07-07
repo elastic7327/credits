b03fe0b912e9a8c04c3ee6bc83736c43b6b77714
elastic7327 <elastic7327@gmail.com>
2018-07-07 20:47:17 +0900

    morden codes

--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -6,7 +6,10 @@ Github: https://github.com/yourname
 Description:
 """
 
+from mixer.backend.flask import mixer
+
 from src.tests.common.base import TestBaseClass
+from src.models.users import User
 
 
 class TestModelsClass(TestBaseClass):
@@ -15,4 +18,11 @@ class TestModelsClass(TestBaseClass):
 
     def test_smoke_test(self):
 
-        assert 1 is not 1
+        assert 1 is 1, "Should be equal"
+
+    def test_create_fake_users(self):
+
+        for x in range(5):
+            mixer.blend(User)
+
+        assert User.query.count() == 5, "Suppose to be five"
