5cb5d66b6fa22fc5af90c1443513d19e9b049dd5
elastic7327 <elastic7327@gmail.com>
2018-07-08 03:40:38 +0900

    cmt

--- src/flaskr.py
+++ src/flaskr.py
@@ -6,16 +6,36 @@ Github: https://github.com/elastic7327
 Description: . . . .
 """
 
+
+import sys
+import select
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)  # pylint: disable=invalid-name
+app = Flask('__name__')  # pylint: disable=invalid-name
 db = SQLAlchemy(app)  # pylint: disable=invalid-name
 
 
-@app.route('/')
-def hello_world():
-    """
-    doc
-    """
-    return 'Hello, World!'
+# @click.option('<', prompt=True)
+# @click.argument('filename')
+# def hello(filename):
+# click.echo(filename)
+
+
+def credit():
+
+    if select.select([sys.stdin, ], [], [], 0.0)[0]:
+        print("Have data! stdin")
+
+    else:
+
+        print("No data! file data")
+        filename = sys.argv[-1]
+        with open(filename, 'r') as f:
+            for line in f:
+                print(line)
+
+
+if __name__ == '__main__':
+    credit()
