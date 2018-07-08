5b9b04089248d5169c70859163701f8e6634fa3c
elastic7327 <elastic7327@gmail.com>
2018-07-08 21:16:36 +0900

    cmt

--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -1,8 +1,8 @@
 """
 File: test_models.py
-Author: Me
-Email: yourname@email.com
-Github: https://github.com/yourname
+Author: Daniel
+Email: elastic7327@email.com
+Github: https://github.com/elastic7327
 Description:
 """
 
@@ -21,7 +21,6 @@ class TestModelsClass(TestBaseClass):
 
     """Test case docstring."""
 
-    @pytest.mark.skip(reason="skip it for a moment")
     def test_smoke_test(self):
         """
             import Path 확인을 위한 스모크 테스트입니다.
@@ -45,9 +44,12 @@ class TestModelsClass(TestBaseClass):
             구현한 luhn 함수를 users 모델 validate에 등록을 해서
             테스트 합니다.
         """
-        mixer.blend(User, card_no=self.valid_card_no1, username='Daniel', limitation=10000, balance=0)
+        mixer.blend(
+                User,
+                card_no=self.valid_card_no1,
+                username='Daniel',
+                limitation=10000, balance=0)
 
-    @pytest.mark.skip(reason="skip it for a moment")
     def test_duplicate_card_no_check(self):
         """
             카드를 두번 등록을해서, 중복테스트를 합니다.
@@ -71,7 +73,6 @@ class TestModelsClass(TestBaseClass):
         except exc.IntegrityError as e:
             db.session().rollback()
 
-    @pytest.mark.skip(reason="skip it for a moment")
     def test_transaction_models_charge_case_without_transaction_zone(self):
         """
             트렌젝션 모델테스트 without Transaction' zone
@@ -85,22 +86,21 @@ class TestModelsClass(TestBaseClass):
         tr = mixer.blend(
                 Transaction,
                 user_id=daniel.id,
-                type=0,
+                type=1,
                 tram=1000,
-                label=f'{daniel.username} Charged {amount} at the Sumgo Online Shop'
+                label=f'{daniel.username} Credit {amount} at the Sumgo Online Shop'
             )
 
         daniel.balance = (daniel.balance - amount)
 
         db.session.commit()
 
-        assert daniel.balance == -amount, "1000원을 Charge 했기 때문에 -1000원 이여야합니다."
+        assert daniel.balance == -amount, "1000원을 Credit 했기 때문에 -1000원 이여야합니다."
         assert tr.tram == amount, f"{amount}, 값만큼 트렌젝션이 발생해야합니다."
-        assert tr.type == 0, "Charge 이므로 타입이 0으로 발생해야합니다."
+        assert tr.type == 0, "Credit 이므로 타입이 0으로 발생해야합니다."
 
         Transaction.query.count() == 1
 
-    @pytest.mark.skip(reason="skip it for a moment")
     def test_transaction_models_credit_case_without_transaction_zone(self):
         """
             트렌젝션 모델테스트 without Transaction' zone
@@ -112,7 +112,9 @@ class TestModelsClass(TestBaseClass):
                 balance=0,
                 username=self.test_username2, limitation=10000)
 
-        chris = User.query.filter_by(card_no=self.valid_card_no2, username=self.test_username2).first()
+        chris = User.query.filter_by(
+                card_no=self.valid_card_no2,
+                username=self.test_username2).first()
 
         # 트렌젝션을 생성시키고, 어떤 행위인지, 그리고 어마운트와 트렌젝션 타입에 대해서, 유저의 밸런스를
         # 적절하게 수정을 해줍니다.
@@ -123,7 +125,7 @@ class TestModelsClass(TestBaseClass):
                 user_id=chris.id,
                 type=1,
                 tram=1000,
-                label=f'{chris.username} Credit {amount}'
+                label=f'{chris.username} Charge {amount}'
             )
 
         chris.balance = (chris.balance + amount)
@@ -132,7 +134,7 @@ class TestModelsClass(TestBaseClass):
 
         chris = User.query.filter_by(card_no=self.valid_card_no2).first()
 
-        assert chris.balance == amount, "1000원을 Credit 했기 때문에 +1000원 이여야합니다."
+        assert chris.balance == amount, "1000원을 Charge 했기 때문에 +1000원 이여야합니다."
         assert tr.tram == amount, f"{amount}, 값만큼 트렌젝션이 발생해야합니다."
         assert tr.type == 1, "Charge 이므로 타입이 0으로 발생해야합니다."
 
@@ -157,7 +159,7 @@ class TestModelsClass(TestBaseClass):
             user_id=trump_user_a.id,
             type=1,
             tram=5000,
-            label=f'{trump_user_a.username} Credit {amount}'
+            label=f'{trump_user_a.username} Charge {amount}'
         )
 
         trump_user_a.balance += amount
@@ -170,7 +172,9 @@ class TestModelsClass(TestBaseClass):
         # 트럼프 고객은 남은 잔액을 확인해본 결과 자신의 크레딧은 4,000 달러가 있음을 숨고 시스템을
         # 통해서 확인을 할 수가 있습니다.
 
-        trump_user_b = User.query.filter_by(card_no=self.valid_card_no3, username=self.test_username3).first()
+        trump_user_b = User.query.filter_by(
+                card_no=self.valid_card_no3,
+                username=self.test_username3).first()
 
         next_amount = 1000
 
@@ -179,8 +183,7 @@ class TestModelsClass(TestBaseClass):
             user_id=trump_user_b.id,
             type=0,
             tram=next_amount,
-            # label=f'{trump_user_b.username} Credit {amount}'
-            label=f'{trump_user_b.username} Charged {next_amount} at the Sumgo Shop'
+            label=f'{trump_user_b.username} Credit {next_amount} at the Sumgo Shop'
         )
 
         trump_user_b.balance -= next_amount
@@ -195,23 +198,23 @@ class TestModelsClass(TestBaseClass):
         try:
 
             # 트랙젝션 시작
-
             # 다행히도 숨고팀의 서버쪽에서는, 프론트 쪽에서 이런 잘못된 요청이 들어오더라도, 서버사이드에서 더블체크를 하고 있기 때문에
             # 이런 요청이 오더라도 걱정 할 필요가 없습니다.
-
             # 트럼프 고객님의 한도가 초과된 트렌젝션은 유효하지 않기 때문에,
             # 트렌젝션(오류) 기록에는 남지만, 트럼프 고객님의 정보에서는 아무런 변화가 없습니다.
 
-            trump_user_c = User.query.filter_by(card_no=self.valid_card_no3, username=self.test_username3).first()
+            trump_user_c = User.query.filter_by(
+                    card_no=self.valid_card_no3,
+                    username=self.test_username3).first()
 
             after_next_amount = 20000
 
             mixer.blend(
                 Transaction,
                 user_id=trump_user_b.id,
-                type=0,
+                type=1,
                 tram=next_amount,
-                label=f'{trump_user_c.username} Charged {after_next_amount} at the Sumgo Shop'
+                label=f'{trump_user_c.username} Credit {after_next_amount} at the Sumgo Shop'
             )
 
             trump_user_c.balance -= after_next_amount
@@ -229,6 +232,8 @@ class TestModelsClass(TestBaseClass):
         except AssertionError as e:
 
             print("OverCharged Error!")
+
+        # 적절하게 트렌젝션이 롤백이 되었는지 다시 확인을 합니다.
 
         trump_user_d = User.query.filter_by(
                 card_no=self.valid_card_no3,
