3a82795901d0f82a88300ab911a4b2600161c183
elastic7327 <elastic7327@gmail.com>
2018-07-25 16:56:09 +0900

    addd

--- src/tests/test_luhn.py
+++ src/tests/test_luhn.py
@@ -13,28 +13,16 @@ from src.modules.luhn import luhn
 
 
 class TestLuhnModules(TestBaseClass):
-
     """Test case docstring."""
     def test_simple_luhn_validate_case_1(self):
         res = luhn("121232232")
         assert res is not True
 
     def test_simple_luhn_validate_case_2(self):
-        """
-            테스트를 위해서 제가 실제로 사용하는
-            신용카드 및 체크카드번호를 입력했습니다.
-            평가가 끝나면 꼭 해당 코드들은 파기 부탁드립니다.
-        """
-        assert luhn("4028571169702107") is True
+        assert luhn("2028571169702107") is not True
 
     def test_simple_luhn_validate_case_3(self):
-        """
-            테스트를 위해서 제가 실제로 사용하는
-            신용카드 및 체크카드번호를 입력했습니다.
-            평가가 끝나면 꼭 해당 코드들은 파기 부탁드립니다.
-        """
-        assert luhn("9440038135573607") is True
-
+        assert luhn("2028571169702107") is not True
         # 위의 신용카드 번호에서 한자리수를 지워본다 => False
         # tail 숫자 7을 삭제했다
         assert luhn("944003813557360") is False
